509a0e82efea541be9aee543f5179576a473de86
MaxNikoskinen <56909209+MaxNikoskinen@users.noreply.github.com>
2021-01-21 12:16:17 +0200

    Harjoitus 2

---
+++ Harjoitukset/Harjoitus2/Harjoitus2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1 = new Opiskelija("Matti", "TVT1234", 0);
+
+            opiskelija1.TulostaData();
+
+            opiskelija1.MuokkaaOpintopisteitä(5);
+
+            opiskelija1.TulostaData();
+
+            Console.ReadLine();
+        }
+    }
+}
